d991cde994ad7a35c4e2232884d51b1560914bf8
nelsonmatenda <matendafigueiredo@hotmail.com>
2024-05-20 14:42:46 +0100

    melhorando o substr

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: nfigueir <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/20 08:55:29 by nfigueir          #+#    #+#             */
-/*   Updated: 2024/05/20 09:47:45 by nfigueir         ###   ########.fr       */
+/*   Updated: 2024/05/20 14:42:04 by nfigueir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,27 +23,24 @@ char	*ft_substr(char const *s, unsigned int start, size_t len)
 		return (NULL);
 	i = 0;
 	j = 0;
-	while (s[i])
+	while (s[i] && i >= start && j < len)
 	{
-		if (i >= start && j < len)
-		{
-			sub[j] = s[i];
-			j++;
-		}
+		sub[j] = s[i];
+		j++;
 		i++;
 	}
 	sub[j] = 0;
 	return (sub);
 }
 
-//int	main(int ac, char **av)
-//{
-//	if (ac == 4)
-//	{
-//		char *sub = ft_substr(av[1], atoi(av[2]), atoi(av[3]));
-//		printf("%s | %s | %s\n", sub, av[2], av[3]);
-//	}
-//	else
-//		printf("3 arg");
-//	return 0;
-//}
+/*int	main(int ac, char **av)
+{
+	if (ac == 4)
+	{
+		char *sub = ft_substr(av[1], atoi(av[2]), atoi(av[3]));
+		printf("%s | %s | %s\n", sub, av[2], av[3]);
+	}
+	else
+		printf("3 arg");
+	return 0;
+}*/
